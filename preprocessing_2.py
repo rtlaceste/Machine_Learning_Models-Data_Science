c84b0f63a0eb56013bc076eb579bc5dc8be5bd1b
Troy Laceste <73194494+rtlaceste@users.noreply.github.com>
2020-10-28 00:56:54 -0700

    Update preprocessing_2.py

--- preprocessing_2.py
+++ preprocessing_2.py
@@ -3,15 +3,13 @@
 
 # In[1]:
 
-
-# import all libraries needed
 import numpy as np
 import pandas as pd
 import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 
-# the custom scaler class 
+
 class CustomScaler(BaseEstimator,TransformerMixin): 
     
     def __init__(self,columns,copy=True,with_mean=True,with_std=True):
@@ -33,7 +31,7 @@ class CustomScaler(BaseEstimator,TransformerMixin):
         return pd.concat([X_not_scaled, X_scaled], axis=1)[init_col_order]
 
 
-# create the special class that we are going to use from here on to predict new data
+
 class absenteeism_model():
       
         def __init__(self, model_file, scaler_file):
@@ -43,19 +41,19 @@ class absenteeism_model():
                 self.scaler = pickle.load(scaler_file)
                 self.data = None
         
-        # take a data file (*.csv) and preprocess it in the same way as in the lectures
+        
         def load_and_clean_data(self, data_file):
             
-            # import the data
+            
             df = pd.read_csv(data_file,delimiter=',')
-            # store the data in a new variable for later use
+            
             self.df_with_predictions = df.copy()
             # drop the 'ID' column
             df = df.drop(['ID'], axis = 1)
-            # to preserve the code we've created in the previous section, we will add a column with 'NaN' strings
+            
             df['Absenteeism Time in Hours'] = 'NaN'
 
-            # create a separate dataframe, containing dummy values for ALL avaiable reasons
+            
             reason_columns = pd.get_dummies(df['Reason for Absence'], drop_first = True)
             
             # split reason_columns into 4 types
@@ -64,14 +62,13 @@ class absenteeism_model():
             reason_type_3 = reason_columns.loc[:,18:21].max(axis=1)
             reason_type_4 = reason_columns.loc[:,22:].max(axis=1)
             
-            # to avoid multicollinearity, drop the 'Reason for Absence' column from df
+            # Fixing multicollinearity issue
             df = df.drop(['Reason for Absence'], axis = 1)
             
             # concatenate df and the 4 types of reason for absence
             df = pd.concat([df, reason_type_1, reason_type_2, reason_type_3, reason_type_4], axis = 1)
             
-            # assign names to the 4 reason type columns
-            # note: there is a more universal version of this code, however the following will best suit our current purposes             
+            # assign names to the 4 reason type columns          
             column_names = ['Date', 'Transportation Expense', 'Distance to Work', 'Age',
                            'Daily Work Load Average', 'Body Mass Index', 'Education', 'Children',
                            'Pet', 'Absenteeism Time in Hours', 'Reason_1', 'Reason_2', 'Reason_3', 'Reason_4']
@@ -109,13 +106,13 @@ class absenteeism_model():
             df = df[column_names_upd]
 
 
-            # map 'Education' variables; the result is a dummy
+           
             df['Education'] = df['Education'].map({1:0, 2:1, 3:1, 4:1})
 
-            # replace the NaN values
+            
             df = df.fillna(value=0)
 
-            # drop the original absenteeism time
+            
             df = df.drop(['Absenteeism Time in Hours'],axis=1)
             
             # drop the variables we decide we don't need
@@ -124,10 +121,9 @@ class absenteeism_model():
             # we have included this line of code if you want to call the 'preprocessed data'
             self.preprocessed_data = df.copy()
             
-            # we need this line so we can use it in the next functions
             self.data = self.scaler.transform(df)
     
-        # a function which outputs the probability of a data point to be 1
+        
         def predicted_probability(self):
             if (self.data is not None):  
                 pred = self.reg.predict_proba(self.data)[:,1]
@@ -139,8 +135,7 @@ class absenteeism_model():
                 pred_outputs = self.reg.predict(self.data)
                 return pred_outputs
         
-        # predict the outputs and the probabilities and 
-        # add columns with these values at the end of the new data
+        
         def predicted_outputs(self):
             if (self.data is not None):
                 self.preprocessed_data['Probability'] = self.reg.predict_proba(self.data)[:,1]
